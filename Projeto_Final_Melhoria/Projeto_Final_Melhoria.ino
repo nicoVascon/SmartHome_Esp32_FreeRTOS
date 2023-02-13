ce25ee34f745b5d5b1f9af527daa86c7b2acdf48
dnicolauit <112620599+dnicolauit@users.noreply.github.com>
2023-02-13 15:58:29 +0000

    sensores done

--- Projeto_Final_Melhoria/Projeto_Final_Melhoria.ino
+++ Projeto_Final_Melhoria/Projeto_Final_Melhoria.ino
@@ -7,32 +7,54 @@
 #include "esp_system.h"
 //---------------- PINS -----------------//
 
-#define LED_PIN 25    //led
-
+#define LM35_Pin 4    //temperature lm35
+#define GAS_PIN 15    //analog gas 
+#define LIGHT_PIN 26   //ambient light sensor
 //-------- ADDITIONAL CONSTANTS ---------//
 #define ADC_RESOLUTION 10
 
 //--------------- TASKS -----------------//
-void vLEDPWM(void *pvParameters);
+void vTemperature(void *pvParameters);
+void vAnalogGas(void *pvParameters);
+void vAmbientLight(void *pvParameters);
 
 void setup() {
-  xTaskCreatePinnedToCore(vLEDPWM, "PWM for LED Task", 1024, NULL, 1, NULL, 1);
+  Serial.begin(9600);//Set Baud Rate to 9600 bps
+  xTaskCreatePinnedToCore(vTemperature, "Temperature Measurement Task", 1024, NULL, 1, NULL, 1);
+  xTaskCreatePinnedToCore(vAnalogGas, "Analog Gas Measurement Task", 1024, NULL, 1, NULL, 1);
+  xTaskCreatePinnedToCore(vAmbientLight, "Ambient Light Measurement Task", 1024, NULL, 1, NULL, 1);
   analogReadResolution(ADC_RESOLUTION);
 }
 
-void vLEDPWM(void *pvParameters) {
+void vTemperature(void *pvParameters) {
+  int analogTemp;
+  float analogTemp_voltage;
   for (;;) {
-    for (int dutyCycle = 0; dutyCycle <= (pow(2,ADC_RESOLUTION)); dutyCycle++){
-      ledcWrite(LED_PIN, dutyCycle);
-      vTaskDelay(1 / portTICK_PERIOD_MS);
-    }
-    for (int dutyCycle = (pow(2,ADC_RESOLUTION)); dutyCycle >= 0; dutyCycle--){
-      ledcWrite(LED_PIN, dutyCycle);
-      vTaskDelay(1 / portTICK_PERIOD_MS);
-    }
+    analogTemp = analogRead(LM35_Pin);
+    analogTemp_voltage=(500*analogTemp) /4096;
+    Serial.print("Temp:"); //Display the temperature on Serial monitor
+    Serial.println(analogTemp_voltage);
+    vTaskDelay(250 / portTICK_PERIOD_MS);
   }
 }
 
+void vAnalogGas(void *pvParameters) {
+  int val;
+  for (;;) {
+    val = analogRead(GAS_PIN);
+    Serial.println(val);
+    vTaskDelay(250 / portTICK_PERIOD_MS);
+  }
+}
+
+void vAmbientLight(void *pvParameters) {
+  int analogLight;
+  for (;;) {
+    analogLight = analogRead(LIGHT_PIN);
+    Serial.println(analogLight);
+    vTaskDelay(250 / portTICK_PERIOD_MS);
+  }
+}
 
 
 void loop() {
